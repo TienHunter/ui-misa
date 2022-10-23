8aafb5381f3493e4a263225268e12ef147de101f
TienHunter <tienkbtnhp@gmail.com>
2022-10-23 18:49:09 +0700

    update 23/10/2022 thêm mới nhân viên

--- js/employee.js
+++ js/employee.js
@@ -1,3 +1,7 @@
+/**
+ * document load xong tài nguyên
+ * author VDTien (18/10/2022)
+ */
 $(document).ready(function () {
    initEvents();
    loadData();
@@ -10,10 +14,33 @@ function initEvents() {
    try {
       // toggle Sidebar
       toggleSidebar();
-      // open Form
-      openForm();
-      // hide Form
-      hideForm();
+      // handle click btn thêm mới nhân viên
+      $(".main-header__actions .btn__add-employee").click(function () {
+         // open Form
+         openForm();
+      });
+
+      // hanlde click btn close form
+      $(".form-header__action .m-icon--close").click(function () {
+         $(".m-popup-wrapper.warning-close-form").css("display", "flex");
+         $(
+            ".m-popup-wrapper.warning-close-form .popup-footer__left .btn-sub"
+         ).click(function () {
+            $(".m-popup-wrapper.warning-close-form").css("display", "none");
+         });
+         $(
+            ".m-popup-wrapper.warning-close-form .popup-footer__right .btn-sub"
+         ).click(function () {
+            $(".m-popup-wrapper.warning-close-form").css("display", "none");
+            hideForm();
+         });
+         // hide Form
+      });
+      // hanlde click btn Hủy form
+      $(".form-footer__left .btn-sub").click(function () {
+         // hide Form
+         hideForm();
+      });
       // handleSaveOnClick
       handleSaveOnClick();
    } catch (error) {
@@ -33,11 +60,14 @@ async function loadData() {
          url: "https://amis.manhnv.net/api/v1/Employees",
          success: function (response) {
             numberReords = response.length;
+            // empty table current
+            $(".m-table__content tbody").empty();
             for (const item of response) {
                // console.log(item);
                const {
                   EmployeeCode,
                   EmployeeName,
+                  Gender,
                   GenderName,
                   DateOfBirth,
                   IdentityNumber,
@@ -47,9 +77,6 @@ async function loadData() {
                   BankName,
                   BankBranchName,
                } = item;
-               // console.log(item);
-               // hiển thị lên table
-
                $(".m-table__content tbody").append(
                   `
                      <tr>
@@ -59,7 +86,9 @@ async function loadData() {
                         <td>${EmployeeCode}</td>
                         <td>${EmployeeName}</td>
                         <td>${GenderName}</td>
-                        <td>${DateOfBirth}</td>
+                        <td class="m-text-center">${convertDateOfBirth(
+                           DateOfBirth
+                        )}</td>
                         <td>${IdentityNumber}</td>
                         <td>${IdentityPlace}</td>
                         <td>${DepartmentName}</td>
@@ -68,24 +97,43 @@ async function loadData() {
                         <td>${BankBranchName}</td>
                         <td class="td-anchor td-anchor--end m-d-flex-auto">
 
-                              <a href="" class>Sửa</a>
+                              <a href="#" class>Sửa</a>
 
-                              <div
+                                 <div
                                  class="m-icon m-icon--dropdown"
                                  style="position:relative"
-                              >
-                                 <ul class='dropdownlist'>
-                                    <li class='dropdown__item'>Nhân bản</li>
-                                    <li class='dropdown__item'>Xóa</li>
-                              
-                                 </ul>
-                        
-                           </div>
+                                 >
+                                    <ul class='dropdownlist'>
+                                       <li class='dropdown__item'>Nhân bản</li>
+                                       <li class='dropdown__item'>Xóa</li>
+                                 
+                                    </ul>
+                                 </div>
                         </td>
                      </tr>
                   `
                );
             }
+            $(".td-anchor.td-anchor--end a").click(function () {
+               openForm();
+            });
+            $(document).click(function (e) {
+               let _this;
+               $(".td-anchor.td-anchor--end .m-icon--dropdown").click(
+                  function () {
+                     _this = $(this);
+                     $(".td-anchor.td-anchor--end .m-icon--dropdown").css(
+                        "border",
+                        "1px solid #fff"
+                     );
+                     $(this).css("border", "1px solid #0075c0");
+                  }
+               );
+               if (_this) {
+                  if (!_this.is(e.target))
+                     _this.css("border", "1px solid #fff");
+               }
+            });
             $(".m-table_pagination .pagination__left b").text(numberReords);
          },
       });
@@ -120,19 +168,47 @@ function toggleSidebar() {
  * open Form employee
  * author VDTien (18/10/2022)
  */
-function openForm() {
-   $(".main-header__actions .btn__add-employee").click(function () {
-      $(".m-form-wrapper").css("display", "flex");
+async function openForm(item = {}) {
+   $(".m-form-wrapper").css("display", "flex");
+   $("#ma_nhan_vien").focus();
+   // load dữ liệu departments vào form
+   let departments = await getDataDepartments();
+   if (departments.length > 0) {
+      $("#ten_don_vi ul").empty();
+      for (const item of departments) {
+         const { DepartmentId, DepartmentName } = item;
+
+         $("#ten_don_vi ul").append(
+            `
+            <li class="dropdown__item" departmentId=${DepartmentId}>${DepartmentName}</li>
+            `
+         );
+      }
+   }
+   // click dropdown departments
+   $("#ten_don_vi button").click(function () {
+      $("#ten_don_vi ul").fadeToggle();
+   });
+
+   // select item droplist departments
+   $("#ten_don_vi ul li").click(function () {
+      let _this = this;
+      const { DepartmentId } = $(this).attr("departmentId");
+      $("#ten_don_vi ul li").removeClass("active");
+      $(this).addClass("active");
+      $("#ten_don_vi ul").fadeOut();
+      $("#ten_don_vi input").attr({
+         value: `${$(_this).text()}`,
+         departmentId: `${$(_this).attr("departmentId")}`,
+      });
    });
 }
 /**
  * hide Form employee
  * author VDTien (18/10/2022)
  */
-function hideForm() {
-   $(".form-header__action .m-icon--close").click(function () {
-      $(".m-form-wrapper").hide();
-   });
+function hideForm(item = {}) {
+   $(".m-form-wrapper").css("display", "none");
 }
 /**
  * xử lý dữ liệu và trả về
@@ -146,10 +222,11 @@ function handleSaveOnClick() {
          //thu thập dữ liệu
          let maNhanVien = $("#ma_nhan_vien").val();
          let tenNhanVien = $("#ten_nhan_vien").val();
-         // let donVi = "Phòng nhân sự";
-         let maDonVi = "142cb08f-7c31-21fa-8e90-67245e8b283e";
+         let maDonVi = $("#ten_don_vi input").attr("departmentId");
+         // let maDonVi = "142cb08f-7c31-21fa-8e90-67245e8b283e";
          let chucDanh = $("#chuc_danh").val();
-         let ngaySinh = $("#ngay_sinh").val();
+         // let ngaySinh = $("#ngay_sinh").val();
+         let ngaySinh = "10/10/2020";
          let gioiTinh;
          let cccd = $("#cccd").val();
          let ngayCapCCCD = $("#ngay_cap_cccd").val();
@@ -166,27 +243,13 @@ function handleSaveOnClick() {
             ".m-form-wrapper .textfield__container--radio input[type='radio'][name='gender-form']"
          ).each(function () {
             if ($(this).prop("checked") == true) {
-               // gioiTinh = $(this).attr("value");
-               switch ($(this).attr("value")) {
-                  case "0":
-                     gioiTinh = "nam";
-                     break;
-                  case "1":
-                     gioiTinh = "nữ";
-                     break;
-                  case "2":
-                     gioiTinh = "khác";
-                     break;
-                  default:
-                     gioiTinh = "";
-                     break;
-               }
+               gioiTinh = $(this).attr("value");
             }
          });
          let employee = {
             EmployeeCode: maNhanVien,
             EmployeeName: tenNhanVien,
-            GenderName: gioiTinh,
+            Gender: gioiTinh,
             PositionName: chucDanh,
             DateOfBirth: ngaySinh,
             IdentityNumber: cccd,
@@ -218,10 +281,75 @@ function handleSaveOnClick() {
                return res.json();
             })
             .then((res) => {
-               if (statusCode === 400) {
-                  alert(res.data.userMsg);
+               switch (statusCode) {
+                  case 400:
+                     $(".m-popup-wrapper.error-submit-form").css(
+                        "display",
+                        "flex"
+                     );
+                     $(
+                        ".m-popup-wrapper.error-submit-form .popup-body__right"
+                     ).text(res.userMsg);
+                     $(
+                        ".m-popup-wrapper.error-submit-form .popup-footer__right .btn-primary"
+                     ).click(function () {
+                        $(".m-popup-wrapper.error-submit-form").css(
+                           "display",
+                           "none"
+                        );
+                     });
+                     break;
+                  case 201: // thêm nhân viên mới thành công
+                     // ẩn form
+                     hideForm();
+                     // hiện toast message
+                     $("body").append(
+                        `
+                              <div class="m-toast-wrapper">
+                              <div class="toast-message">
+                              <span class="toast-icon toast-icon--succes">
+                                 <i class="fa-solid fa-circle-check"></i>
+                              </span>
+                              <div class="toast-content">
+                                 <span class="toast-title toast-title--success">Thành công!</span>
+                                 <span class="toast-text">Công việc đã bị xóa</span>
+                              </div>
+                              <a href="" class="toast-action">Hoàn tác</a>
+                              <button class="toast-close">
+                                 <i class="fa-solid fa-xmark"></i>
+                              </button>
+                           </div>
+                              </div>
+                           `
+                     );
+
+                     $(".m-toast-wrapper").show(function () {
+                        $(this).animate({ right: "40px" });
+                     });
+                     $(".m-toast-wrapper .toast-close").click(function () {
+                        $(".m-toast-wrapper").animate(
+                           { right: "-500px" },
+                           function () {
+                              $(this).hide();
+                           }
+                        );
+                     });
+                     setTimeout(function () {
+                        $(".m-toast-wrapper").animate(
+                           { right: "-500px" },
+                           function () {
+                              $(this).hide();
+                           }
+                        );
+                     }, 3000);
+
+                     loadData();
+                     //load lại dữ liệu
+                     break;
+                  default:
+                     alert("Đã xảy ra lỗi vui lòng thử lại sau");
+                     break;
                }
-               console.log(res);
             })
             .catch((err) => {
                console.log(err);
@@ -237,7 +365,7 @@ function handleSaveOnClick() {
 function validateData() {
    let isValid = true;
    // các thông tin bắt buộc nhập
-   $(".textfield__input--require").each(function () {
+   $(".textfield__input--required").each(function () {
       // console.log(element);
       let value = $(this).val();
       if (!value) {
@@ -318,9 +446,54 @@ function hamdleClickDropdownActionTable() {
          .children()
          .hide();
       $(this).children().toggle();
-      $(this).parent().css("z-index", 10);
+      $(this).parent().css("z-index", 1);
    });
-   // $(document).click(function (e) {
-   //    if (!_this.is(e.target)) _this.children().hide();
-   // });
+
+   $(document).click(function (e) {
+      if (_this) {
+         if (!_this.is(e.target)) _this.children().hide();
+      }
+   });
+}
+/**
+ * thực hiện get dữ liệu các bộ phận
+ * @returns danh sách các bộ phận
+ * author VDTien(23/10/2022)
+ */
+async function getDataDepartments() {
+   try {
+      let departments = [];
+      await $.ajax({
+         type: "GET",
+         url: "https://amis.manhnv.net/api/v1/Departments",
+         success: function (response) {
+            departments = [...response];
+         },
+      });
+      // console.log(departments);
+      return departments;
+   } catch (error) {
+      console.log(error);
+   }
+}
+/**
+ *
+ * @param {date} date
+ * @returns cáu trúc DD/MM/YYYY
+ */
+function convertDateOfBirth(date) {
+   if (checkForamtDate(date)) {
+      if (date) {
+         date = new Date(date);
+         let day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+         let month =
+            date.getMonth() < 9 ? "0" + date.getMonth() : date.getMonth() + 1;
+         let year = date.getFullYear();
+         return `${day}/${month}/${year}`;
+      }
+   }
+   return "";
+}
+function checkForamtDate(date) {
+   return new Date(date) !== "Invalid Date" && !isNaN(new Date(date));
 }
